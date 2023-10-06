1e661678f10fcc54be5531e24d0813abb59f049d
bomoche022 <bomoche022@student.wethinkcode.co.za>
2023-10-06 06:55:59 +0200

    refactored logic of data extraction and handling

--- main.py
+++ main.py
@@ -1,30 +1,5 @@
-weather_data = []
+import data_extraction
 
-with open('weather-data.txt', 'r') as file:
-    lines = file.readlines()
 
-for line in lines[1:]:
-    parts = line.strip().split(',')
-    weather_data.append(parts)
-
-consistent_data = {}
-
-date = ''
-city = ''
-temp_str = ''
-humidity_str = ''
-condition = ''
-
-for row in weather_data:
-    date, city, temp_str, humidity_str, condition = row
-    
-    if temp_str == "" or humidity_str == "":
-        temp_str = 0
-        humidity_str = 0
-
-    consistent_data[city] = date, temp_str , humidity_str, condition
-    print(consistent_data)
-
-    
-
-        
+data = data_extraction.read_file()
+data_extraction.handled_data(data)
